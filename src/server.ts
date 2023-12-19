8d249a761e4e22185d79a4bab942712a70a2019b
rakib <rakibhosen2244@gmail.com>
2023-12-19 14:21:23 +0600

    mongoose and express and typescript and cost and env mode

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import config from "./app/config";
+
+const mongoose = require('mongoose');
+
+async function main() {
+    await mongoose.connect(config.database_url);
+}
+
+app.listen(config.port, () => {
+    console.log(`Example app listening on port ${config.port}`)
+})
